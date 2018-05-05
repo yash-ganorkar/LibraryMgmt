92004df35beeb65dae4cc7782be52a5c3b4eefc3
yash-ganorkar <yash.ganorkar@outlook.com>
2018-05-04 20:40:01 -0500

    Commit #3: Added remaining entites for the system.

--- LibraryMgmt/LibraryData/Migrations/20180505010327_Add initial entity models.Designer.cs
+++ LibraryMgmt/LibraryData/Migrations/20180505010327_Add initial entity models.Designer.cs
@@ -8,9 +8,10 @@ using LibraryData;
 namespace LibraryData.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    partial class LibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20180505010327_Add initial entity models")]
+    partial class Addinitialentitymodels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
