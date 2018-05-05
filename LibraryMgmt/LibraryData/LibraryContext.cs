bc723ba05c8fb14e1365d80f7c24464891b0ca9c
yash-ganorkar <yash.ganorkar@outlook.com>
2018-05-04 19:17:28 -0500

    Commit #2: Created Patrons table using Entity Framework.

---
+++ LibraryMgmt/LibraryData/LibraryContext.cs
@@ -0,0 +1,13 @@
+﻿using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LibraryData
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Patron> Patrons { get; set; }
+    }
+}
---
+++ LibraryMgmt/LibraryData/Migrations/LibraryContextModelSnapshot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using LibraryData;
+
+namespace LibraryData.Migrations
+{
+    [DbContext(typeof(LibraryContext))]
+    partial class LibraryContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("LibraryData.Models.Patron", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Address");
+
+                    b.Property<DateTime>("DateOfBirth");
+
+                    b.Property<string>("FirstName");
+
+                    b.Property<string>("LastName");
+
+                    b.Property<string>("TelephoneNumber");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Patrons");
+                });
+        }
+    }
+}
